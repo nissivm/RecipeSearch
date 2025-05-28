fbd46b2292010a16952cad348e1a3ce81fc82971
Nissi Miranda <nissi.miranda@edreamsodigeo.com>
2025-05-28 16:25:03 +0100

    Initial Commit

---
+++ RecipeSearch/RecipeSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeSearchApp.swift
+//  RecipeSearch
+//
+//  Created by nissi.miranda on 28/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct RecipeSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipeSearchTests/RecipeSearchTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeSearchTests.swift
+//  RecipeSearchTests
+//
+//  Created by nissi.miranda on 28/05/2025.
+//
+
+import Testing
+@testable import RecipeSearch
+
+struct RecipeSearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
