caf52bf2b43e98b2afd334e1ecbd68c9865d37f0
Nissi Miranda <nissi.miranda@edreamsodigeo.com>
2025-05-31 16:24:39 +0100

    Created view modifier for task operator, so recipes are fetched only on first appearance of screen

---
+++ RecipeSearch/View/Extensions/View+taskFirstAppear.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+extension View {
+    func taskFirstAppear(_ onTaskFirstAppearAction: @escaping () async -> Void) -> some View {
+        modifier(OnTaskFirstAppearModifier(onTaskFirstAppearAction))
+    }
+}
+
+// MARK: - OnTaskFirstAppearModifier
+
+/// A view modifier that performs an asynchronous task only once when the view appears.
+struct OnTaskFirstAppearModifier: ViewModifier {
+    private let onTaskFirstAppearAction: () async -> Void
+
+    @State private var hasAppeared = false
+
+    init(_ onTaskFirstAppearAction: @escaping () async -> Void) {
+        self.onTaskFirstAppearAction = onTaskFirstAppearAction
+    }
+
+    func body(content: Content) -> some View {
+        content
+            .task {
+                guard !hasAppeared else { return }
+                hasAppeared = true
+                await onTaskFirstAppearAction()
+            }
+    }
+}
